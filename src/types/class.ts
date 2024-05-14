7e690417ef0be72f716c997128337fe038fdb400
Zero <1203970284@qq.com>
2024-05-14 21:49:34 +0800

    feat: able

--- src/types/class.ts
+++ src/types/class.ts
@@ -1,3 +1,6 @@
 export type Class<T = unknown> = new (...args: unknown[]) => T;
+export type Classable<T = unknown> = Class<T> | T;
 export type AbstractClass<T = unknown> = abstract new (...args: unknown[]) => T;
+export type AbstractClassable<T = unknown> = AbstractClass<T> | T;
 export type Constructor<T = unknown> = Class<T> | AbstractClass<T>;
+export type Constructorable<T = unknown> = Constructor<T> | T;
