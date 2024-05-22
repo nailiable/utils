08815566c2a7cebd7c2a037e8979197ad8deb0b5
Zero <1203970284@qq.com>
2024-05-23 00:14:03 +0800

    feat: add type

--- src/types/able.ts
+++ src/types/able.ts
@@ -1,5 +1,7 @@
 export type Promisable<T> = T | Promise<T>;
 
+export type InferPromise<T> = T extends Promise<infer U> ? U : T;
+
 export type Awaitable<T> = T | PromiseLike<T>;
 
 export type Nullable<T> = T | null | undefined;
